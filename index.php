2c247553ee21198c9a4fcccbb15558a077563218
Julien Desrosiers <juliend2@gmail.com>
2019-02-18 20:24:18 -0500

    forgot to pass the received parameters to the other function

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include 'config.php';
 
-function sequentially_list_records($api_key, $table_name, $view_name, $offset = null) {
+function sequentially_list_records($api_key, $db_id, $table_name, $view_name, $offset = null) {
     $context = stream_context_create(array(
     'http' => array(
         'method' => 'GET',
@@ -19,17 +19,17 @@ function sequentially_list_records($api_key, $table_name, $view_name, $offset = 
         $params['offset'] = $offset;
     }
     $query_string = http_build_query($params);
-    $json_response = file_get_contents("https://api.airtable.com/v0/appZJFyErEhuZNkPN/$table_name?$query_string", false, $context);
+    $json_response = file_get_contents("https://api.airtable.com/v0/$db_id/$table_name?$query_string", false, $context);
     return json_decode($json_response);
 }
 
 // Do things with $func, and return the cardinality of the results
-function do_with_records($api_key, $table_name, $view_name, $func) {
+function do_with_records($api_key, $db_id, $table_name, $view_name, $func) {
     $offset = null;
     $records = array();
     $count = 0;
     do {
-        $response = sequentially_list_records(API_KEY, 'bonheur', 'mainview', $offset);
+        $response = sequentially_list_records(API_KEY, $db_id, $table_name, $view_name, $offset);
         $count += count($response->records);
         if (is_callable($func)) {
             $func($response->records);
@@ -38,7 +38,7 @@ function do_with_records($api_key, $table_name, $view_name, $func) {
     return $count;
 }
 
-$n_records = do_with_records(API_KEY, 'bonheur', 'mainview', function($records){
+$n_records = do_with_records(API_KEY, 'appZJFyErEhuZNkPN', 'bonheur', 'mainview', function($records){
     echo '<dl>';
     foreach ($records as $record) {
         echo '<dt>';
